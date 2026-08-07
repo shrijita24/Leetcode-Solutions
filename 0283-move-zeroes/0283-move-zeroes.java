a8e617f24f55ef5a168d5946bb060e43eeb1e308
Shrijita Bhattacharyya <shrijitabhattacharyya24@gmail.com>
2026-08-08 00:22:12 +0530

    Time: 2 ms (92.1%), Space: 47.7 MB (57.75%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int i=0;
-            for(int j=0;j<nums.length;j++)
+        for(int j=0;j<nums.length;j++)
+        {
+            if(nums[j]!=0)
             {
-                if(nums[j]!=0)
-                {
-                    int temp=nums[i];
-                    nums[i]=nums[j];
-                    nums[j]=temp;
-                    i++;
-                }
+                int temp=nums[j];
+                nums[j]=nums[i];
+                nums[i]=temp;
+                i++;
             }
-        
+        }
     }
 }
+/*We have an array containing zero and non-zero elements. We want to put all the non-zero elements first and the zeroes at the end. i points to the position where the next non-zero element should be placed, while j scans the entire array. Whenever j finds a non-zero element, we swap nums[j] with nums[i], because i is the position where that non-zero belongs. Then we increment i so it points to the position for the next non-zero element.*/
