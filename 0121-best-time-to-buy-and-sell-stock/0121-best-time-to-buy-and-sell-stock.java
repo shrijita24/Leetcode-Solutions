0d0738d886292e0a8f2337cf00787144bd666082
Shrijita Bhattacharyya <shrijitabhattacharyya24@gmail.com>
2026-08-13 00:27:38 +0530

    Time: 3 ms (5.81%), Space: 94.5 MB (51.81%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,14 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
-       int minPrice=prices[0];//minimum price for buying
-       int maxProfit=0; 
+       int minPrice=prices[0];
+       int maxPrice=0;
        for(int i=1;i<prices.length;i++)
        {
-        minPrice=Math.min(prices[i],minPrice);
-        int profit=prices[i]-minPrice;//profit if sold today
-        maxProfit=Math.max(profit,maxProfit);
+        minPrice=Math.min(minPrice,prices[i]);
+        int profit=prices[i]-minPrice;
+        maxPrice=Math.max(maxPrice,profit);
        }
-       return maxProfit;
+       return maxPrice;
     }
 }
 /*We find the minimum price to buy the stock,find current profit by substracting buying price from selling price and keep maximising the profit unless we get the max, return the max profit */
