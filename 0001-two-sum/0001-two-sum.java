ad1a03a7d8a7319081e1dabcb3e33183356b8eee
Shrijita Bhattacharyya <shrijitabhattacharyya24@gmail.com>
2026-09-02 14:33:28 +0530

    Time: 2 ms (99.34%), Space: 47 MB (59.41%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,7 +5,9 @@ class Solution {
         {
             int complement=target-nums[i];
             if(map.containsKey(complement))
+            {
                 return new int[]{map.get(complement),i};
+            }
             map.put(nums[i],i);
         }
         return new int[]{};
