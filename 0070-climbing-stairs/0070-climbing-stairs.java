76354c6b9845ba6712d8f7de33ba2e3f6f0ffb51
Shrijita Bhattacharyya <shrijitabhattacharyya24@gmail.com>
2026-09-07 04:16:23 +0530

    Time: 0 ms (100%), Space: 42.1 MB (35.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,17 +2,14 @@ class Solution {
     public int climbStairs(int n) {
         int first=1;
         int second=2;
-        int current=0;
         if(n==1)
             return first;
-        if(n==2)
-            return second;
         for(int i=3;i<=n;i++)
-         {  
-            current=first+second;
+        {
+            int current=first+second;
             first=second;
             second=current;
-         }
-            return current;
+        }
+        return second;   
     }
 }
